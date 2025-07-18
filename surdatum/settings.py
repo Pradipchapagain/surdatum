72998a9ac8e6e5e64d1831bd866526ff4ecf8fa3
Pradipchapagain <pradipchapagain@gmail.com>
2025-07-18 17:45:51 +0000

    Fix MIDDLEWARE and add Procfile for Render

--- surdatum/settings.py
+++ surdatum/settings.py
@@ -16,8 +16,13 @@ INSTALLED_APPS = [
     'maps',
 ]
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    ...
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ALLOW_ALL_ORIGINS = True
